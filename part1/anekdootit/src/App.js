3dec8b925fe16b3c8eec78886d6e33eda7dc2776
Markus Kaihola <markus.kaihola@helsinki.fi>
2020-09-26 09:55:40 +0300

    anecdotes

--- part1/anekdootit/src/App.js
+++ part1/anekdootit/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [selected, setSelected] = useState(0)
+  let random = 0
+
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
+  const [ votes, setVotes ] = useState(new Array(anecdotes.length).fill(0))
+
+  const handleClick = (e) => {
+    random = Math.floor(Math.random() * 6)
+    setSelected(random)
+  }
+
+  const highest = votes.indexOf(Math.max(...votes))
+
+  const handleVote = (e) => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[highest]}</p>
+      <p>has {votes[highest]} votes</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
