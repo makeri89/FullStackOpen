f0476178d1ec0b73a811a4c623b7828e057aea60
Markus Kaihola <markus.kaihola@helsinki.fi>
2020-09-28 22:13:40 +0300

    fixes

--- part4/blogilista/index.js
+++ part4/blogilista/index.js
@@ -5,27 +5,37 @@
 
 // const server = http.createServer(app)
 
-// server.listen(config.PORT, () => {
+// app.listen(config.PORT, () => {
 //     logger.info(`Server running on port ${config.PORT}`)
 // })
-
+const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
-const mongoUrl = 'mongodb+srv://fullstack:fullstack@cluster0.kums7.mongodb.net/Bloglist?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
